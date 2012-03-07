7bf7160aa8ab93bef2b403c54b7589203e88d181
Chris <github@adminwerk.de>
2012-03-08 00:11:36 +0100

    a lot of changes to make it work

--- modules/nginx/manifests/vhost.pp
+++ modules/nginx/manifests/vhost.pp
@@ -1,30 +1,52 @@
 #
-#  define: nginx::vhost::001
+#  define: nginx::vhost
 #  author: chris@adminwerk.de
 # version: 1.0.0
 #    date: 07.03.2012
 #    info:
 #
-#   usage: 
+#   usage: nginx::vhost::site { "domain.tld": }
 #
 
-define nginx::vhost:001 (
-
-	# [GLOBALS]
-	$nginx_user,
-	$nginx_worker_processes,
-	$nginx_pid,
-	$nginx_worker_connections) {
-
-	include nginx
-
-	file {
-		"/etc/nginx/nginx.conf":
+class nginx::vhost {
+	
+	define site (
+		$siteName = $title,
+		$vhost_port,
+		$upstream,
+		$server_1_ip,
+		$server_2_ip,
+		$server_3_ip,
+		$vhost_index,
+		$vhost_proxy_cache,
+		$vhost_proxy_cache_valid ) {
+	
+	file { 
+		"/etc/nginx/sites-available/$title.conf":
+			ensure => present,
 			owner => "root",
 			group => "root",
-			mode => "644",
-			content => template("nginx/nginx.conf.erb"),
-			ensure => present
+			mode => 644,
+			content => template("nginx/vhost.erb");
+		
+		"/var/www":
+			ensure => "directory",
+			owner => "www-data",
+			group => "www-data",
+			mode => 770;
+
+		"/var/www/$title":
+			ensure => present,
+			owner => "www-data",
+			group => "www-data";
+		
+		"/etc/nginx/sites-enabled/$title.conf":
+			ensure => symlink,
+			target => "/etc/nginx/sites-available/$title.conf",
+			notify => Service['nginx'];
+
 	}
+
+  }
 
 }
