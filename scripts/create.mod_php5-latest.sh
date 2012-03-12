a0ef45fa5d7aab1340311b1b69c1b2eb12d737f1
adminwerk <github@adminwerk.de>
2012-03-12 10:43:39 +0100

    added dependency

--- scripts/create.mod_php5-latest.sh
+++ scripts/create.mod_php5-latest.sh
@@ -181,7 +181,7 @@ cd $SRCTARGET
 
 # Dependencies, conf and compiler needed
 # -------------------------------------------------------------------------
-$APTGET -m -q -y install apache2-threaded-dev libc-client2007e-dev
+$APTGET -m -q -y install apache2-threaded-dev libc-client2007e-dev libkrb5-dev
 $APTGET -m -q -y install checkinstall libbz2-dev libcurl4-openssl-dev libcurl3 libjpeg62-dev libfreetype6-dev
 $APTGET -m -q -y install comerr-dev krb5-multidev libgssrpc4 libidn11-dev libkadm5clnt-mit7 libt1-dev libxslt1-dev
 $APTGET -m -q -y install libkadm5srv-mit7 libkdb5-4 libkrb5-dev pkg-config libpng12-dev libxpm-dev libxslt1.1
