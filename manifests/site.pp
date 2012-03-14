d6fba625c04001e7b63b38a5c39c4a2b8b3206dc
Chris <github@adminwerk.de>
2012-03-14 12:54:19 +0100

    import new variables connected to users

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,19 @@
 #
 #    name: site.pp
 #  author: chris@adminwerk.de
-# version: 1.0.7
-#    date: 08.03.2012
-#
+# version: 1.0.11
+#    date: 14.03.2012
+#    info: this file may NOT be deployed to a public place for 
+#          various and obvious reasons
 
 import "classes/*.pp"
 import "nodes/*.pp"
 
 # variables valid for a couple of server
 import "/etc/puppet/variables/global.pp"
+
+# import of user credentials
+import "/etc/puppet/variables/_user_creds.pp"
 
 # set default $PATH for all exec
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
