267a0d18c571f1f88619726cb0b04defc68f8e26
Chris <github@adminwerk.de>
2012-03-20 17:34:59 +0100

    corrected sample usage information

--- modules/php/manifests/setini.pp
+++ modules/php/manifests/setini.pp
@@ -6,15 +6,31 @@
 # Parameters:   
 #
 # Actions:
-#   Creates /etc/php5/php.ini
+#   Creates /etc/php5/php.ini with defined parameters
 #
 # Requires:
 #   Requires PHP to be installed prior to creating php.ini
 #
 # Sample Usage:
-#	ini { "$php::inst_prefix/etc/php.ini": }
 #
-
+#		php::setini { "setup-php-ini":
+#		php_ini_safe_mode => "Off",
+#		php_ini_max_execution_time =>"600",
+#		php_ini_max_inout_time => "300",
+#		php_ini_memory_limit => "256M",
+#		php_ini_error_reporting => "E_ALL & ~E_NOTICE",
+#		php_ini_error_log => "/var/log/php5.log",
+#		php_ini_register_globals => "Off",
+#		php_ini_post_max_size => "50M",
+#		php_ini_include_path  => ".:/usr/share/ZendFramework/library",
+#		php_ini_upload_max_filesize => "50M",
+#		php_ini_default_socket_timeout => "60",
+#		php_ini_date_timezone => "Europe/Berlin",
+#		php_ini_smtp_host => ' ',
+#		php_ini_smtp_port => "25",
+#		php_ini_sendmail_path => ' ',
+#		php_ini_extension_dir => '/usr/local/php/lib/php/extensions/no-debug-non-zts-20090626'}
+#
 define php::setini (
 
 		$php_ini_safe_mode,
