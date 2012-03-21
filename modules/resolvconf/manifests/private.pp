a97145b1e675d1f78995276c864e21243b836cac
Chris <github@adminwerk.de>
2012-03-21 10:29:24 +0100

    load a different resolv.conf from the stack of files

--- modules/resolvconf/manifests/private.pp
+++ modules/resolvconf/manifests/private.pp
@@ -1,10 +1,23 @@
 #
-#   class: resolvconf::private
 #  author: chris@adminwerk.de
-# version: 1.0.0
-#    date: 10.03.2012
+# version: 1.0.1
+#    date: 19.03.2012
 #
 
+# Definition: resolvconf::private
+#
+#	Installs the resolv.conf file with private DNS informations
+#
+# Parameters:   
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+#	include resolvconf::private
+#
 class resolvconf::private {
 	file { "/etc/resolv.conf":
 		source => "puppet:///data/resolvconf/resolv.conf.private",
